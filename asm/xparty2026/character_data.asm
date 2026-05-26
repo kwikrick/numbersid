ee02b7c799a693070152bd6c7d63a5c3512116c9
kwikrick <rickvandermeiden@gmail.com>
2026-05-26 23:14:02 +0200

    alternate sinebob characters

--- asm/xparty2026/character_data.asm
+++ asm/xparty2026/character_data.asm
@@ -1,7 +1,60 @@
-
-
 // character data
 
+// version 2
+
+character_data1:
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00010000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+
+character_data2:
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00010000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+
+character_data3:
+.byte %00000000
+.byte %00000000
+.byte %00111000
+.byte %00111000
+.byte %00111000
+.byte %00000000
+.byte %00000000
+.byte %00000000
+
+character_data4:
+.byte %00000000
+.byte %01111100
+.byte %01111100
+.byte %01111100
+.byte %01111100
+.byte %01111100
+.byte %00000000
+.byte %00000000
+
+character_data5:
+.byte %11111110
+.byte %11111110
+.byte %11111110
+.byte %11111110
+.byte %11111110
+.byte %11111110
+.byte %11111110
+.byte %00000000
+
+
+// version 1
+/*
 character_data1:
 .byte %00000000
 .byte %00000000
@@ -52,3 +105,4 @@ character_data5:
 .byte %01111100
 .byte %00111000
 .byte %00000000
+*/
